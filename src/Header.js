5605bb85cf12c2be44f3cb0dd551b43b7f4686b7
evedes <eduardo.vedes@gmail.com>
2017-08-20 19:04:26 +0100

    updated js files and deleted index.css

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+import fcclogo from './img/freeCodeCamp.jpg'
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <img className="logo" src={fcclogo} alt="freecodecamp logo" />
+            <p className="hfont1">Game of Life</p>
+            <p className="hfont2">4th React Challenge</p>
+        </div>
+    )
+}
+
+export default Header
+  
